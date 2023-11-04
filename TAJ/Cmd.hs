3273339b3b3c5cf6960e6269911f88a0a8c2e18c
combi2k2 <cumbee2k2@gmail.com>
2023-11-04 12:27:40 +0100

    add interface for using command

--- TAJ/Cmd.hs
+++ TAJ/Cmd.hs
@@ -1,4 +1,11 @@
 module Cmd where
+import Bin
 
-data Cmd = Go_Left | Go_Right | Go_Down | Stand_Still | Use_Tracker |Quit
+data Cmd = Go_Left | Go_Right | Go_Down | Stand_Still | Use_Tracker | Quit
   deriving (Show,Read)
+
+move :: BinZip -> Cmd -> Maybe BinZip
+move t Go_Left = (go_left t)
+move t Go_Right = (go_right t)
+move t Go_Down = (go_down t)
+move t Stand_Still = Just t
